d824640579b82a875c7041d94058c3e3a2f0e7b0
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-03-07 12:05:39 +0300

    fix query parameters

--- app/src/main/java/com/itis/group11801/fedotova/smartfasting/net/ApiService.kt
+++ app/src/main/java/com/itis/group11801/fedotova/smartfasting/net/ApiService.kt
@@ -11,8 +11,7 @@ interface ApiService {
     suspend fun getNews(
         @Query("country") country: String = "us",
         @Query("category") category: String = "health",
-        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
-        @Query("q") q: String = "food"
+        @Query("apiKey") api: String = BuildConfig.API_KEY
     ): News
 }
 // http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=39ca608664a741e390df7aabf5b0e7c5
